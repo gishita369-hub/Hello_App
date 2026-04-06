d483a387b21f40ac595e4ae633f6afc6c8fe08d2
gishita <gishita369@gmail.com>
2026-04-06 15:23:50 +0530

    Added UC2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 public class Main {
-    static void main() {
-
-        System.out.println("Hello World");
-
+    public static void main(String args[]) {
+        //UC2
+        if(args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello " + name);
         }
+
+
+    }
 }
 
