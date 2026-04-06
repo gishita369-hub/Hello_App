fb95712447774db206af716296b9b863cded09ea
gishita <gishita369@gmail.com>
2026-04-06 15:29:53 +0530

    Added UC6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,17 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
-        // UC5
+        // UC6
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String nameInput = sc.nextLine();
-        System.out.println("Hello " + nameInput);
+        System.out.println("How many names?");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Enter name:");
+            String name1 = sc.nextLine();
+            System.out.println("Hello " + name1);
+        }
     }
 }
 
