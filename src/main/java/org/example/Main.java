e4da893b6566e6446b91dffd2df0936bbdb64101
gishita <gishita369@gmail.com>
2026-04-06 15:24:57 +0530

    Added UC3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
 public class Main {
     public static void main(String args[]) {
-        //UC2
-        if(args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
+        //UC3
+        if(args.length == 0) {
+            System.out.println("Hello User");
+        } else {
+            System.out.println("Hello " + args[0]);
         }
-
-
     }
 }
 
