ffce9d848d13dc6c7ae197652f46cb656f60f0ea
gishita <gishita369@gmail.com>
2026-04-21 14:53:44 +0530

    Added UC7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,29 @@
 package org.example;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
-    public static void main(String args[]) {
-        // UC6
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How many names?");
-        int count = sc.nextInt();
-        sc.nextLine();
 
-        for (int i = 0; i < count; i++) {
-            System.out.println("Enter name:");
-            String name1 = sc.nextLine();
-            System.out.println("Hello " + name1);
+        public static void main(String args[]) {
+
+                // UC7
+                Scanner sc = new Scanner(System.in);
+
+
+                ArrayList<String> names = new ArrayList<String>();
+
+                System.out.println("How many names do you want to store?");
+                int n = sc.nextInt();
+                sc.nextLine();
+
+                for (int i = 0; i < n; i++) {
+                        System.out.println("Enter name:");
+                        String name2 = sc.nextLine();
+                        names.add(name2);
+                }
+
+                System.out.println("Names stored in memory:");
+                for (int i = 0; i < names.size(); i++) {
+                        System.out.println(names.get(i));
+                }
         }
-    }
 }
-
-
